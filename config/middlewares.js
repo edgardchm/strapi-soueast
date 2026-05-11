764fe4cae03c506c218a38d13900ee362669c2c8
Edgard <edgardchm@users.noreply.github.com>
2026-05-11 17:27:28 -0400

    Ajustes de BDD

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,28 +1,41 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: [
-        'http://localhost:8080',
-        'http://127.0.0.1:8080',
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        // Agregá acá la URL de prod cuando corresponda, ej: 'https://soueast.cl'
-      ],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
-      keepHeaderOnError: true,
-    },
-  },
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+const localOrigins = [
+  'http://localhost:8080',
+  'http://127.0.0.1:8080',
+  'http://localhost:3000',
+  'http://127.0.0.1:3000',
+  'http://localhost:5173',
+  'http://127.0.0.1:5173',
 ];
+
+module.exports = ({ env }) => {
+  const fromEnv = (env('CORS_ORIGIN') || '')
+    .split(',')
+    .map((s) => s.trim())
+    .filter(Boolean);
+  const origin = [
+    ...localOrigins,
+    'https://nimble-gaufre-3f7ca9.netlify.app',
+    ...fromEnv,
+  ];
+
+  return [
+    'strapi::logger',
+    'strapi::errors',
+    'strapi::security',
+    {
+      name: 'strapi::cors',
+      config: {
+        origin,
+        headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
+        keepHeaderOnError: true,
+      },
+    },
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ];
+};
