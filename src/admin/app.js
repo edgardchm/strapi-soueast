29c99b20c41b1398c9197dd73663c893950e15db
Edgard <edgardchm@users.noreply.github.com>
2026-06-01 21:14:38 +0000

    Fix: Use React.lazy() for admin extension and correct route path to /admin/import-excel

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,20 +1,23 @@
 import React from 'react';
 
+const ImportExcelPage = React.lazy(() =>
+  import('./extensions/import-excel/pages/ImportExcelPage').then(module => ({
+    default: module.ImportExcelPage
+  }))
+);
+
 export default {
   config: {
     menu: {
       links: [
         {
-          to: '/plugins/import-excel',
+          to: '/admin/import-excel',
           icon: 'upload',
           intlLabel: {
             id: 'import-excel.menu.label',
             defaultMessage: 'Importar Excel',
           },
-          Component: async () => {
-            const { ImportExcelPage } = await import('./extensions/import-excel/pages/ImportExcelPage');
-            return ImportExcelPage;
-          },
+          Component: ImportExcelPage,
         },
       ],
     },
