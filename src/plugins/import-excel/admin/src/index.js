7de94cf5f3912a29bb720ab53d97b72e39ce999b
Edgard <edgardchm@users.noreply.github.com>
2026-06-02 16:03:04 -0400

    Fix: Correct implementation of addMenuLink() according to official Strapi v4 documentation

--- src/plugins/import-excel/admin/src/index.js
+++ src/plugins/import-excel/admin/src/index.js
@@ -1,31 +1,32 @@
 import pluginId from './pluginId';
 import Initializer from './components/Initializer';
 import PluginIcon from './components/PluginIcon';
-import { App } from './pages';
 
 export default {
   register(app) {
-    // Agregar el menú al sidebar
-    const menuItem = {
-      to: `/plugins/${pluginId}`,
-      icon: PluginIcon,
-      intlLabel: {
-        id: `${pluginId}.plugin.name`,
-        defaultMessage: 'Importar Excel',
-      },
-      Component: App,
-    };
-
-    // Usar el método correcto de Strapi para agregar el menú
-    app.menu.push(menuItem);
-
     app.registerPlugin({
       id: pluginId,
       initializer: Initializer,
       isReady: false,
       name: 'Importar Excel',
     });
+
+    app.addMenuLink({
+      to: `/plugins/${pluginId}`,
+      icon: PluginIcon,
+      intlLabel: {
+        id: `${pluginId}.plugin.name`,
+        defaultMessage: 'Importar Excel',
+      },
+      Component: async () => {
+        const component = await import(
+          /* webpackChunkName: "import-excel" */ './pages/App'
+        );
+        return component;
+      },
+      permissions: [],
+    });
   },
 
-  bootstrap() {},
+  bootstrap(app) {},
 };
